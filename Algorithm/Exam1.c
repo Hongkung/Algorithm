500b9e41babf2c83420c46bb65da1d386e567fca
Ryujaehong <woghd4244@gmail.com>
2019-05-02 16:17:03 +0900

    5/2 Osaka

---
+++ Algorithm/Exam1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//사각형 넓이구하기
+
+int main(void)
+{
+	int a, b;
+	
+	scanf("%d %d",&a,&b);
+	
+	printf("%d",a*b);
+	return 0;
+
+}
---
+++ Algorithm/Exam2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b;
+	scanf("%d", &a);
+
+	printf("%d minutes is %d seconds.", a, a * 60);
+
+	return 0;
+}
